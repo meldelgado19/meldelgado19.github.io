3db452e2589daf8bf80a97229fe648546ea3a50e
meldelgado19 <126177294+meldelgado19@users.noreply.github.com>
2023-03-01 14:53:48 -0300

    Add files via upload
    Primera entrega Curriculum

---
+++ function.js
@@ -0,0 +1,20 @@
+const time = new Date().getHours();
+let greeting;
+if (time < 10) {
+  greeting = "Buen día, cuento un poco de mí:";
+} else if (time < 20) {
+  greeting = "Buenas Tardes, cuento un poco de mí:";
+} else {
+  greeting = "Buenas Noches, cuento un poco de mí:";
+}
+document.getElementById("saludo").innerHTML = greeting;
+
+document.getElementById('boton_1').onclick=function(){
+    console.log('capturamos el evento de click');
+    document.getElementById('puestos_anteriores').innerHTML= " Ayudante estudiantil - Facultad de Ciencias Economicas UNT - 2010 a 2014";
+}
+
+document.getElementById('boton_2').onclick=function(){
+    console.log('capturamos el evento de click');
+    document.getElementById('otras_habilidades').innerHTML="Manejo de grupos, gestión de recursos, movilidad propia";
+}
